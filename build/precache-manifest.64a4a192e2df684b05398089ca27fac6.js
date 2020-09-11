8989043015c28b5f94138b3532130e5ab31e481e
QuimSalinas <salinas.volta2@gmail.com>
2020-09-12 00:04:27 +0200

    GH-pages creation

---
+++ build/precache-manifest.64a4a192e2df684b05398089ca27fac6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bf7671084341d832def5e99f10ecf7",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "0fa9bc68ba8bbb52d8d7",
+    "url": "/react-colors/static/css/2.2a4460b4.chunk.css"
+  },
+  {
+    "revision": "e09ffa5721e403e96d3f",
+    "url": "/react-colors/static/css/main.36a7d862.chunk.css"
+  },
+  {
+    "revision": "0fa9bc68ba8bbb52d8d7",
+    "url": "/react-colors/static/js/2.cbec28aa.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-colors/static/js/2.cbec28aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e09ffa5721e403e96d3f",
+    "url": "/react-colors/static/js/main.b969c3dc.chunk.js"
+  },
+  {
+    "revision": "bb07e0b96fc8d3ac97e1",
+    "url": "/react-colors/static/js/runtime-main.c895aa97.js"
+  },
+  {
+    "revision": "6fd115e263807f8a830e8ffcfc7dea6d",
+    "url": "/react-colors/static/media/bg.6fd115e2.svg"
+  }
+]);
